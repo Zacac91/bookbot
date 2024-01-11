3cefc553d282aacb743488815c5f15727cf3b963
zacac91 <z.colbran@gmail.com>
2024-01-11 14:37:00 +0000

    added final book report print out and archived previous print outputs

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ def main():
    
     letters_count= {}
     for letter in low_text:
-        if letter in letters_count:
-            letters_count[letter] += 1
-        else:
-            letters_count[letter] = 1
+        if letter.isalpha():
+            if letter in letters_count:
+                letters_count[letter] += 1
+            else:
+                letters_count[letter] = 1
 
-    print(text)
-    print(f"{word_count} words found in the document")
-    print (f"{len(low_text)} total characters in the document")
-    print (letters_count)
+    char_list = [[k,v] for k, v in letters_count.items()]
+    char_list.sort(key=lambda x: x[1], reverse=True)
+    book_report = "\n".join([f"The character '{item[0]}' was found {item[1]} times" for item in char_list])
 
+    #print (text)
+    #print (f"{word_count} words found in the document")
+    #print (f"{len(low_text)} total characters in the document")
+    #print (letters_count)
+    
+    print (f"--- Begin report for {book_path} --- \nThe document has {word_count} words, made up of {len(low_text)} characters.\n{book_report}\n--- End of Report ---")
 
 def get_book_text(path):
     with open(path) as f:
