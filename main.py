1f10b7d2c2dd29448e71c43ccca058e770b253e1
zacac91 <z.colbran@gmail.com>
2024-01-11 11:55:54 +0000

    added total character count and ondividual character count to the document

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     words = text.split()
-    count = len(words)
+    low_text = text.lower()
+    word_count = len(words)
+   
+    letters_count= {}
+    for letter in low_text:
+        if letter in letters_count:
+            letters_count[letter] += 1
+        else:
+            letters_count[letter] = 1
+
     print(text)
-    print(f"{count} words found in the document")
+    print(f"{word_count} words found in the document")
+    print (f"{len(low_text)} total characters in the document")
+    print (letters_count)
 
 
 def get_book_text(path):
